77a6c7b9b6451d94424e55aa0f9623588fcb0345
Katya Marincheva <katya.marincheva@outlook.com>
2016-02-10 17:41:41 +0200

    corrected check box toggle script

--- HW JavaScript DOM & Events/Problem 1. Webinar registration form/script.js
+++ HW JavaScript DOM & Events/Problem 1. Webinar registration form/script.js
@@ -1,11 +1,11 @@
-function toggleVisibility() {
+function toggleVisibility(adres) {
     var deliverSame = document.getElementById('invoice');
-    var deliveryAddress = document.getElementById('invoiceDetails');
+    var deliveryAddress = document.getElementById(adres);
     if (deliverSame.checked) {
         deliveryAddress.style.visibility = 'visible';
     } else {
         deliveryAddress.style.visibility = 'hidden';
     }
 }
-toggleVisibility();
+
 
