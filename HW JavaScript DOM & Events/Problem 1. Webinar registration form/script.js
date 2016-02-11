0c7c71255824bc0394c7c087d12295cef9e539f3
Katya Marincheva <katya.marincheva@outlook.com>
2016-02-11 14:33:40 +0200

    adding function attachCheckboxHandlers()

--- HW JavaScript DOM & Events/Problem 1. Webinar registration form/script.js
+++ HW JavaScript DOM & Events/Problem 1. Webinar registration form/script.js
@@ -1,5 +1,16 @@
-function toggleVisibility(checkBox, address) {
-    var checkBox = document.getElementById(checkBox);
+'use strict';
+
+function attachCheckboxHandlers(checkBoxLocation) {
+    // get reference to element with the particular checkbox
+    var el = document.getElementById(checkBoxLocation);
+
+    el.onclick = function() {
+        toggleVisibility('invoice', 'invoiceDetails');
+    }
+}
+
+function toggleVisibility(checkBoxLocation, address) {
+    var checkBox = document.getElementById(checkBoxLocation);
     var deliveryAddress = document.getElementById(address);
     if (checkBox.checked) {
         deliveryAddress.style.visibility = 'visible';
@@ -7,3 +18,6 @@ function toggleVisibility(checkBox, address) {
         deliveryAddress.style.visibility = 'hidden';
     }
 }
+
+attachCheckboxHandlers('invoice');
+
